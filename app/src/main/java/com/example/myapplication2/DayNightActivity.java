23e6d04b13b22298215b31c7f91dc70aae902fd6
PotapovaMarina <potapova_me@inbox.ru>
2021-06-16 18:55:14 +0300

    Добавила кнопку Назад на 2 активити

--- app/src/main/java/com/example/myapplication2/DayNightActivity.java
+++ app/src/main/java/com/example/myapplication2/DayNightActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ToggleButton;
 
@@ -9,6 +11,7 @@ import androidx.appcompat.app.AppCompatDelegate;
 
 public class DayNightActivity extends AppCompatActivity {
     private ToggleButton dayOrNightButton;
+    private Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +20,16 @@ public class DayNightActivity extends AppCompatActivity {
 
 
         dayOrNightButton = findViewById(R.id.day_night_button);
+        backButton = findViewById(R.id.back_button_settings);
+
         dayOrNightButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 AppCompatDelegate.setDefaultNightMode(isChecked ? AppCompatDelegate.MODE_NIGHT_YES : AppCompatDelegate.MODE_NIGHT_NO);
             }
+        });
+        backButton.setOnClickListener(v -> {
+            finish();
         });
     }
 }
